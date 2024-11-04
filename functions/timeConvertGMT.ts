e263242e1149799d60770a1bcdad96887a8d9b3c
ismedom <Khavudom.123@gmail.com>
2024-11-05 01:18:24 +0700

    ---

--- functions/timeConvertGMT.ts
+++ functions/timeConvertGMT.ts
@@ -1,8 +1,20 @@
 //
 
 const timeConvertInGMT = (time: string): string => {
-    const date = new Date(time);
+    const timeZone = new Date().getTimezoneOffset() / 60;
+
+    const timeFormating = time.split(".").slice(0, 1) + `000${expectFormatingValue(-1 * timeZone)}:00`;
+
+    const date = new Date(timeFormating);
     return date.toUTCString();
 };
+
+function expectFormatingValue(number: number) {
+    // const number = -2;
+    const formattedNumber =
+        number < 0 ? "-" + Math.abs(number).toString().padStart(2, "0") : number.toString().padStart(2, "0");
+
+    return formattedNumber;
+}
 
 export default timeConvertInGMT;
